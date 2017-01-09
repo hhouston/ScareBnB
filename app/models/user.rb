4501dd68ca34386f5b75a8c04a54d344b8a291a5
App Academy Student <student@appacademy.io>
2017-01-09 15:17:35 -0800

    able to login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   end
 
   def self.find_by_credentials(email, password)
-    user = User.find_by(username: email)
+    user = User.find_by(email: email)
     return nil unless user
     user.password_is?(password) ? user : nil
   end
