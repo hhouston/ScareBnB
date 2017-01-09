8721c48a940673d7d7c36586883ca8838cf81e73
App Academy Student <student@appacademy.io>
2017-01-09 15:10:12 -0800

    adds bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "static_pages#root"
 
-  namespace :api, defaults { format:json } do
+  namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create]
   end
