6c69061eba20e83b7ad0a009b1e9f5ce18815c07
App Academy Student <student@appacademy.io>
2017-01-09 17:45:33 -0800

    adds app, root, and greeting component to frontend/components

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,9 +1,26 @@
 import { merge } from 'lodash/merge';
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT,
+  RECEIVE_ERRORS
+} from '../actions/session_actions';
 
+const _nullUser = Object.freeze({
+  currentUser: null,
+  errors: []
+});
 
-const sessionReducer = (state, action) => {
+const sessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
   switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, _nullUser, { currentUser });
+    case LOGOUT:
+      return merge({}, _nullUser);
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, _nullUser, { errors });
     default:
       return state;
   }
