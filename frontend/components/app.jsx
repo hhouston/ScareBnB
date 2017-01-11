6ea40c6ea342f958a970ba2d3f8ad77392648fe1
hhouston <hmhouston7@gmail.com>
2017-01-10 17:17:00 -0800

    bootstrap buttons tested/working

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
+import BButton from 'react-bootstrap/lib/Button';
 
 const App = ({ children }) => (
   <div>
     <GreetingContainer />
     <h1>Scare BnB</h1>
+    <BButton>test</BButton>
     { children }
   </div>
 );
