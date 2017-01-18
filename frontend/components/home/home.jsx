f3dba142f1feb74f56a56c5fbd788d532d9c2d68
hhouston <hmhouston7@gmail.com>
2017-01-18 01:28:55 -0800

    implement google autocomplete on home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,12 +1,45 @@
 import PlacesIndexContainer from '../places/places_container';
-
 import React from 'react';
+import PlacesAutocomplete, { geocodeByAddress } from 'react-places-autocomplete';
+// <PlacesAutocomplete value={this.state.address} onChange={this.onChange} />
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search_value: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  handleSubmit(e) {
+      e.preventDefault();
+        this.props.search(this.state).then(data => {
+            this.props.router.push(`/places/${data.place.id}`);
+      });
+  }
+
+  update(property) {
+      return e => this.setState({[property]: e.target.value});
+  }
+
+  errors() {
+      if (this.props.errors) {
+          return (this.props.errors.map(error => {
+              return (
+                  <li className="error" key={error}>{error}</li>
+              );
+          }));
+      }
+  }
 
   render() {
     return(
       <div className="home-content">
+        <PlacesAutocomplete className="search-bar" value={this.state.searh_value} onChange={this.update} />
         <PlacesIndexContainer />
       </div>
     );
